f6dec97be9d96ef5d8cb1b3162d73004ffd47251
amhammett <am.hammett@gmail.com>
2022-03-17 06:16:20 +0000

    default if hash not found

--- src/config/defaults.js
+++ src/config/defaults.js
@@ -1,4 +1,6 @@
 import githash from './githash.json';
 
 export const drawerWidth = 240;
-export const hashCode = `#${githash.gitsha.substring(0, 6)}`;
+export const hashCode = githash.gitsha
+  ? `#${githash.gitsha.substring(0, 6)}`
+  : '#ff000';
